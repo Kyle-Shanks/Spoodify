d16ab94e6eaa8ed352fb34c0a29f82da8fde50fa
KJ Shanks <kyle.j.shanks@gmail.com>
2018-09-14 15:44:26 -0400

    Add nav and hover icons

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -12,19 +12,37 @@ const Sidebar = (props) => {
         <li>
           <Link to="/search"
             className={"app-link thick"+((props.match.params.section === "search") ? " active" : "")}>
+            <div className="rela-inline svg-container nav">
+              <svg viewBox="0 0 140 140" className="rela-block svg search">
+                <circle cx="60" cy="60" r="50"/>
+                <path d="M 95 95 L 125 125"/>
+              </svg>
+            </div>
             Search
           </Link>
         </li>
         <li>
           <Link to="/browse"
             className={"app-link thick"+((props.match.params.section === "browse") ? " active" : "")}>
-            Browse
+            <div className="rela-inline svg-container nav">
+              <svg viewBox="0 0 140 140" className="rela-block svg home">
+                <path d="M 20 130 L 20 60 L 70 20 L 120 60 L 120 130 L 85 130 L 85 90 L 55 90 L 55 130 Z"/>
+              </svg>
+            </div>
+            Home
           </Link>
         </li>
         <li>
           <Link to="/collection"
             className={"app-link thick"+((props.match.params.section === "collection") ? " active" : "")}>
-            Collection
+            <div className="rela-inline svg-container nav">
+              <svg viewBox="0 0 140 140" className="rela-block svg collection">
+                <path d="M 10 20 L 10 120"/>
+                <path d="M 40 20 L 40 120"/>
+                <path d="M 70 20 L 115 120"/>
+              </svg>
+            </div>
+            Your Library
           </Link>
         </li>
       </ul>
